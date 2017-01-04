25ec8cd30c82438e333d7fdaf9111f6113335f04
Taylor Boykin <taylorboykin@arrayschool.com>
2017-01-04 16:59:03 -0700

    End of Wed, Jan 4 commit

---
+++ ex32.rb
@@ -0,0 +1,22 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+the_count.each do |number|
+  puts "This is the count #{number}."
+end
+
+fruits.each do |fruit|
+  puts "A type of fruit: #{fruit}"
+end
+
+change.each {|i| puts "I got #{i}"}
+
+elements = []
+
+(0..5).each do |i|
+  puts "Adding #{i} to the list..."
+  elements << i
+end
+
+elements.each {|i| puts "Element was: #{i}"}
